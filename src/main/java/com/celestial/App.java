0c29a4728f54ea8ace40e4a5baa2423524b53c08
Selvyn <swright@celestial.co.uk>
2022-10-14 16:51:23 +0100

    initial push

---
+++ src/main/java/com/celestial/App.java
@@ -0,0 +1,13 @@
+package com.celestial;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
