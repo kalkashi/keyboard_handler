ff7d3d4c1cce4ed4d19cf61ae7f007d3241d0107
Selvyn <swright@celestial.co.uk>
2022-10-26 17:43:24 +0100

    Added another prompt type, so had to had prompt interface

--- src/main/java/com/celestial/App.java
+++ src/main/java/com/celestial/App.java
@@ -10,27 +10,31 @@ import java.util.Scanner;
  * Keyboard Handler
  *
  */
-public class App 
+public class App
 {
-	public static void main(String[] args) 
-	{
-		ArrayList<TextBlock> lines;
 
-		CustomPrompt cp = new CustomPrompt();
-		FunkyPrompt fk = new FunkyPrompt();
-		ArrayList<IElementReader> readers = new ArrayList<>(2);
-		readers.add(new MsgLineReader(fk));
-		readers.add(new MsgElementReader(cp));
-		
-		Runner rnr = new Runner( readers );
-		
-		lines = rnr.run();
+    public static void main(String[] args)
+    {
+        ArrayList<TextBlock> lines;
 
-		for (var tb : lines)
-			System.out.println(tb);
+        CustomPrompt cp = new CustomPrompt();
+        FunkyPrompt fk = new FunkyPrompt();
+        ArrayList<IElementReader> readers = new ArrayList<>(2);
+        readers.add(new MsgLineReader(fk));
+        readers.add(new MsgElementReader(cp));
 
-		lines.forEach(tb -> {
-			System.out.println(tb);
-		});	
-	}
+        Runner rnr = new Runner(readers);
+
+        lines = rnr.run();
+
+        for (var tb : lines)
+        {
+            System.out.println(tb);
+        }
+
+        lines.forEach(tb ->
+        {
+            System.out.println(tb);
+        });
+    }
 }
