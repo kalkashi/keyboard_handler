84ced406a8876a2b38f85ac38100d472d90c947f
Selvyn <swright@celestial.co.uk>
2022-10-26 17:23:54 +0100

    Code refectored and ready to go

--- src/main/java/com/celestial/App.java
+++ src/main/java/com/celestial/App.java
@@ -14,35 +14,23 @@ public class App
 {
 	public static void main(String[] args) 
 	{
-//		ArrayList<TextBlock> lines;
-//
-//		CustomPrompt cp = new CustomPrompt();
-//		FunkyPrompt fk = new FunkyPrompt();
-//		ArrayList<IElementReader> readers = new ArrayList<>(2);
-//		readers.add(new MsgLineReader(fk));
-//		readers.add(new MsgElementReader(cp));
-//		
-//		Runner rnr = new Runner( readers );
-//		
-//		lines = rnr.run();
-//
-//		for (var tb : lines)
-//			System.out.println(tb);
-//
-//		lines.forEach(tb -> {
-//			System.out.println(tb);
-//		});
+		ArrayList<TextBlock> lines;
+
+		CustomPrompt cp = new CustomPrompt();
+		FunkyPrompt fk = new FunkyPrompt();
+		ArrayList<IElementReader> readers = new ArrayList<>(2);
+		readers.add(new MsgLineReader(fk));
+		readers.add(new MsgElementReader(cp));
 		
-		IElementReader er = (is) ->{
-			System.out.println( "Hello from lambda");
-			return null;
-			};
-			
-		fooBar(er);
-	}
-	
-	public static void fooBar(IElementReader rr )
-	{
-		rr.readFromKeyboard(System.in);
+		Runner rnr = new Runner( readers );
+		
+		lines = rnr.run();
+
+		for (var tb : lines)
+			System.out.println(tb);
+
+		lines.forEach(tb -> {
+			System.out.println(tb);
+		});	
 	}
 }
